e51d0dc455239b4e27d374ab074b028d9c36e1a7
Franco Sanguineti <franco.sanguineti@ug.uchile.cl>
2020-04-11 00:04:55 -0400

    new scraper and project scheme

--- src/getdata/scraping/helpers/utils.py
+++ src/getdata/scraping/helpers/utils.py
@@ -1,11 +1,8 @@
 import json
 import datetime
 import subprocess as cmd
-from bs4 import BeautifulSoup as soup
-from urllib.request import urlopen as ureq
 
-
-from constants import GOV_PAGE_URL, REGIONS_JSON_FILE, BASE_PATH
+from .constants import REGIONS_JSON_FILE, BASE_PATH
 
 
 def undotter(string_number):
@@ -13,7 +10,7 @@ def undotter(string_number):
 
 
 def format_date_last_update(date_string):
-    date_str = date_string.replace("*Informe corresponde al ", "").replace(".", "")
+    date_str = date_string.replace("actualizadas al ", "").replace(".", "")
     months = [
         "enero",
         "febrero",
@@ -29,9 +26,9 @@ def format_date_last_update(date_string):
         "diciembre",
     ]
     name_month = date_str.split()[2].lower()
-    day = "{:0>2d}".format(int(date_str.split()[0]))
+    day = "{:0>2d}".format(int(date_str.split()[0]) + 1)
     number_month = "{:0>2d}".format(months.index(name_month) + 1)
-    year = date_str.split()[4]
+    year = "2020"
 
     return "{}/{}/{}".format(number_month, day, year)
 
@@ -47,22 +44,9 @@ def get_regions_info():
     return json.dumps(regions, ensure_ascii=False)
 
 
-def get_minsal_page():
-    # utility to fecth minsal data
-    uclient = ureq(GOV_PAGE_URL)
-    page_html = uclient.read()
-    uclient.close()
-    page_soup = soup(page_html, "html.parser")
-    container = page_soup.find("div", {"class": "contenido"})
-    table = container.table.tbody
-    date = container.p.strong
-    rows = table.findAll("tr")
-
-    return {"rows": rows, "date": date}
-
-
 def git_commit_and_push():
     date = datetime.date.today().strftime("%m/%d/%y")
+    cmd.run("git pull", shell=True, cwd=BASE_PATH)
     cmd.run("git add .", shell=True, cwd=BASE_PATH)
     message = "contagios y muertes al {}".format(date)
     cmd.run("git commit -m '{}'".format(message), shell=True, cwd=BASE_PATH)
---
+++ src/update_files.py
@@ -0,0 +1,3 @@
+from getdata.scraping.update import update_files
+
+update_files()
