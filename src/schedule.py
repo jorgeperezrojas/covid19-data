dd381108932f60fb19fd925bd0e7228da09ca573
Franco Sanguineti <franco.sanguineti@ug.uchile.cl>
2020-04-11 04:58:59 -0400

    actualizado cron

--- src/schedule.py
+++ src/schedule.py
@@ -1,13 +1,15 @@
+import os
 from crontab import CronTab
 from getpass import getuser
 import subprocess as cmd
 
+
 from getdata.scraping.helpers.constants import CRON_EXECUTABLE, VIRTUAL_ENV
 
 user = getuser()
-
+HOME = os.getenv("HOME")
 cron = CronTab(user=user)
 job = cron.new(command="{} {} >> ~/cron.log 2>&1".format(VIRTUAL_ENV, CRON_EXECUTABLE))
 job.setall("00,15,30,45 8-13 * * *")
-cmd.run("rm -f cron.log", shell=True, cwd="~/")
+cmd.run("rm -f cron.log", shell=True, cwd=HOME)
 cron.write()
