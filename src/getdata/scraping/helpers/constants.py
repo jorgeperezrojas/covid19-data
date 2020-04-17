d4ec2d630b06873d16640cccaecd3fd1092519dd
Kelly Wagemann <kelly.andrea97@gmail.com>
2020-04-17 14:29:46 -0400

    agregando constantes json y scope google spreadsheets

--- src/getdata/scraping/helpers/constants.py
+++ src/getdata/scraping/helpers/constants.py
@@ -7,7 +7,9 @@ BASE_PATH = (
 VIRTUAL_ENV = os.getenv("COVID19_VENV")
 GOV_PAGE_URL = "https://www.gob.cl/coronavirus/cifrasoficiales/"
 MINSAL_PAGE_URL = "https://www.minsal.cl/nuevo-coronavirus-2019-ncov/casos-confirmados-en-chile-covid-19/"
+
 REGIONS_JSON_FILE = "{}/src//getdata/scraping/helpers/regions.json".format(BASE_PATH)
+SPREADSHEET_JSON_FILE = "{}/src//getdata/scraping/helpers/covid-chile.json".format(BASE_PATH)
 CONFIRMED_CSV_PATH = "{}/csv/confirmados.csv".format(BASE_PATH)
 DEATHS_CSV_PATH = "{}/csv/muertes.csv".format(BASE_PATH)
 NOTIFICATIONS_CSV_PATH = "{}/csv/notificaciones.csv".format(BASE_PATH)
@@ -16,3 +18,8 @@ PATIENTS_ICU_CSV_PATH = "{}/csv/pacientes_en_uci.csv".format(BASE_PATH)
 PCRS_REGION = "{}/csv/pcrs_region.csv".format(BASE_PATH)
 CRON_EXECUTABLE = "{}/src/update_files.py".format(BASE_PATH)
 NATIONAL_REPORT_PATH = "{}/csv/resumen_nacional.csv".format(BASE_PATH)
+
+SCOPE_FEEDS = "https://spreadsheets.google.com/feeds"
+SCOPE_SPREADSHEET = "https://www.googleapis.com/auth/spreadsheets"
+GOOGLEAPIS_DRIVE_FILE = "https://www.googleapis.com/auth/drive.file"
+GOOGLEAPIS_DRIVE = "https://www.googleapis.com/auth/drive"
