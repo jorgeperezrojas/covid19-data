6013138ec9920a2769d36b97d349dc8e2719a8cf
Kelly Wagemann <kelly.andrea97@gmail.com>
2020-04-19 17:16:16 -0400

    changed virtual env variable in cron job

--- src/schedule_csv.py
+++ src/schedule_csv.py
@@ -4,13 +4,13 @@ from getpass import getuser
 import subprocess as cmd
 
 
-from getdata.scraping.helpers.constants import CRON_EXECUTABLE_CSV, VIRTUAL_ENV
+from getdata.scraping.helpers.constants import CRON_EXECUTABLE_CSV, VIRTUAL_ENV_AUTO_CSV
 
 user = getuser()
 HOME = os.getenv("HOME")
 cron_csv = CronTab(user=user)
 job_csv = cron_csv.new(
-    command="{} {} >> ~/cron_csv.log 2>&1".format(VIRTUAL_ENV, CRON_EXECUTABLE_CSV)
+    command="{} {} >> ~/cron_csv.log 2>&1".format(VIRTUAL_ENV_AUTO_CSV, CRON_EXECUTABLE_CSV)
 )
 job_csv.minute.every(5)
 cmd.run("rm -f cron_csv.log", shell=True, cwd=HOME)
