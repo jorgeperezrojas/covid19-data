96c489664971e4dd82572c0420b54d2678589fea
Kelly <kelly.andrea97@gmail.com>
2020-04-22 14:40:01 -0400

    changed time to run script

--- src/schedule_csv.py
+++ src/schedule_csv.py
@@ -12,6 +12,6 @@ cron_csv = CronTab(user=user)
 job_csv = cron_csv.new(
     command="{} {} >> ~/cron_csv.log 2>&1".format(VIRTUAL_ENV_AUTO_CSV, CRON_EXECUTABLE_CSV)
 )
-job_csv.minute.every(5)
+job_csv.minute.every(30)
 cmd.run("rm -f cron_csv.log", shell=True, cwd=HOME)
 cron_csv.write()
