51154d47c73e570da0e109f5582171b5606c1a19
Franco Sanguineti <franco.sanguineti@ug.uchile.cl>
2020-06-11 14:50:00 -0400

    contagios y muertes al 06/11/2020

--- src/getdata/scraping/helpers/constants.py
+++ src/getdata/scraping/helpers/constants.py
@@ -22,7 +22,7 @@ PCRS_REGION = "{}/csv/pcrs_region.csv".format(BASE_PATH)
 CRON_EXECUTABLE = "{}/src/update_files.py".format(BASE_PATH)
 CRON_EXECUTABLE_CSV = "{}/src/auto_update_csv.py".format(BASE_PATH)
 NATIONAL_REPORT_PATH = "{}/csv/resumen_nacional.csv".format(BASE_PATH)
-INFOGRAM_LINK = "https://infogram.com/cifras-oficiales-10-de-junio-1hzj4oz9p70p2pw"
+INFOGRAM_LINK = "https://infogram.com/cifras-oficiales-11-de-junio-1h7g6k31w5dg2oy"
 
 SCOPE_FEEDS = "https://spreadsheets.google.com/feeds"
 SCOPE_SPREADSHEET = "https://www.googleapis.com/auth/spreadsheets"
