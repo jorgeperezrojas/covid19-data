6c63cefa1ccb0bcfa493ac6aa21f01798ca03110
Franco Sanguineti <franco.sanguineti@ug.uchile.cl>
2020-06-16 13:47:49 -0400

    resumen_nacional manual

--- src/getdata/scraping/helpers/constants.py
+++ src/getdata/scraping/helpers/constants.py
@@ -22,7 +22,7 @@ PCRS_REGION = "{}/csv/pcrs_region.csv".format(BASE_PATH)
 CRON_EXECUTABLE = "{}/src/update_files.py".format(BASE_PATH)
 CRON_EXECUTABLE_CSV = "{}/src/auto_update_csv.py".format(BASE_PATH)
 NATIONAL_REPORT_PATH = "{}/csv/resumen_nacional.csv".format(BASE_PATH)
-INFOGRAM_LINK = "https://infogram.com/cifras-oficiales-15-de-junio-1h7z2lr9rp3y2ow"
+INFOGRAM_LINK = "https://infogram.com/cifras-oficiales-16-de-junio-1hxr4zvqgep52yo"
 
 SCOPE_FEEDS = "https://spreadsheets.google.com/feeds"
 SCOPE_SPREADSHEET = "https://www.googleapis.com/auth/spreadsheets"
