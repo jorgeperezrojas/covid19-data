17eb563ceca6e4df26b4146dce182f2677d790e3
JC Olivares <juancri@gmail.com>
2020-07-11 04:07:41 -0700

    Adding tests

---
+++ test/test.js
@@ -0,0 +1,26 @@
+
+const Code = require('@hapi/code');
+const { DateTime } = require('luxon');
+const Lab = require('@hapi/lab');
+
+const { expect } = Code;
+const lab = exports.lab = Lab.script();
+
+const csvutil = require('./csvutil');
+
+const FILES = [
+	'confirmados.csv',
+	'confirmados_comunas.csv',
+	'confirmados_comunas_interpolado.csv',
+	'encuesta_sochimi.csv',
+	'muertes.csv',
+	'notificaciones.csv',
+	'pacientes_en_uci.csv',
+	'pcrs_region.csv',
+	'resumen_nacional.csv'
+];
+
+csvutil(lab, FILES, csv => {
+	csv.testDate('date year at least 2020', (date, rawDate) => expect(date.year, rawDate).to.to.be.at.least(2020));
+	csv.testDate('date at most today', (date, rawDate) => expect(+date, rawDate).to.to.be.at.most(+DateTime.utc()));
+});
