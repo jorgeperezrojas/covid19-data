6ac3be55fa3a9a74c168887391f34b1883bd4e3e
JC Olivares <juancri@gmail.com>
2020-07-19 01:21:12 -0700

    Simplifying CSV check configuration override

--- test/test.js
+++ test/test.js
@@ -8,23 +8,29 @@ const lab = exports.lab = Lab.script();
 
 const csvutil = require('./csvutil');
 
-const FILES = [
-	'confirmados.csv',
-	'confirmados_comunas.csv',
-	'confirmados_comunas_interpolado.csv',
-	'encuesta_sochimi.csv',
-	'muertes.csv',
-	'notificaciones.csv',
-	'pacientes_en_uci.csv',
-	'pcrs_region.csv',
-	'resumen_nacional.csv'
-];
+const CSV_CHECK_DEFAULT_CONFIG = {
+	delimiter: ',',
+	duplicates: false,
+	emptyLines: true,
+	emptyValues: false,
+	encoding: 'utf8',
+	limit: false,
+	log: true
+};
+const FILES = {
+	'confirmados.csv': CSV_CHECK_DEFAULT_CONFIG,
+	'confirmados_comunas.csv': CSV_CHECK_DEFAULT_CONFIG,
+	'confirmados_comunas_interpolado.csv': CSV_CHECK_DEFAULT_CONFIG,
+	'encuesta_sochimi.csv': CSV_CHECK_DEFAULT_CONFIG,
+	'muertes.csv': CSV_CHECK_DEFAULT_CONFIG,
+	'notificaciones.csv': { ...CSV_CHECK_DEFAULT_CONFIG, emptyValues: true },
+	'pacientes_en_uci.csv': CSV_CHECK_DEFAULT_CONFIG,
+	'pcrs_region.csv': CSV_CHECK_DEFAULT_CONFIG,
+	'resumen_nacional.csv': CSV_CHECK_DEFAULT_CONFIG,
+};
 
 // Check dates
 csvutil(lab, FILES, csv => {
 	csv.testDate('date year at least 2020', (date, rawDate) => expect(date.year, rawDate).to.to.be.at.least(2020));
 	csv.testDate('date at most today', (date, rawDate) => expect(+date, rawDate).to.to.be.at.most(+DateTime.utc()));
 });
-
-// Check specific files that must have values
-csvutil(lab, ['notificaciones.csv'], () => {}, { emptyValues: true });
